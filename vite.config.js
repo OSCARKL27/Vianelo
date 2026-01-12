ffb0332e96de37a48d6e496d31a188cb31d88237
OSCARKL27 <oscarjelaav@gmail.com>
2026-01-12 11:58:00 -0700

    este es

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
+[11:55 AM, 1/12/2026] OL:   └── @paypal/paypal-js@9.0.1
+[11:57 AM, 1/12/2026] OL: import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom']
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', '@paypal/react-paypal-js', '@paypal/paypal-js']
+  },
   build: {
     commonjsOptions: {
       transformMixedEsModules: true
     }
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom']
-  }
-    ssr: {
-    noExternal: ['react', 'react-dom', '@paypal/react-paypal-js']
   }
 })
