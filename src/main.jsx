c44f41cdc478837431f8bb73a16a1437ac7c62c8
OSCARKL27 <oscarjelaav@gmail.com>
2026-01-12 10:55:40 -0700

    corregido

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles.css'
 
-// 👇 Importa el AuthProvider que creaste
 import { AuthProvider } from './context/AuthContext'
 import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PayPalScriptProvider
       options={{
